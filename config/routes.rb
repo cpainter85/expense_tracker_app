d9fe0865466ef9bbb3cd94f043099c307744730c
Charles Painter <cpainter85@gmail.com>
2018-07-28 17:51:55 -0400

    Adds basic crud for categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "accounts#index"
   resources :accounts, except: [:delete]
-  resources :categories, only: [:index]
+  resources :categories, except: [:show]
 end
