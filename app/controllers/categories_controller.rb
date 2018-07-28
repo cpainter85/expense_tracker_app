00ca494d900662f7b69411356935b42f487d23c1
Charles Painter <cpainter85@gmail.com>
2018-07-28 17:51:55 -0400

    Adds basic crud for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,34 @@
 class CategoriesController < ApplicationController
-  def index  
+  def index 
+    @categories = Category.order(:name) 
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: "#{@category.name} was successfully created"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "#{@category.name} was successfully updated"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
---
+++ spec/requests/categories_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Categories" do
+  describe "POST /categories" do
+    let(:category_attributes) { FactoryBot.attributes_for(:category).with_indifferent_access }
+    
+    it "creates a new Category" do
+      post "/categories", params: { category: category_attributes }
+      expect(response).to redirect_to(categories_path)
+      expect(Category.last.attributes).to include(category_attributes)
+    end
+  end
+
+  describe "PUT /categories/:id/" do
+    let(:category) { create(:category, name: "Foo") }
+    let(:updated_attributes) { { name: "Bar" } }
+    
+    it "updates a Category" do
+      put "/categories/#{category.id}", params: { category: updated_attributes }
+      expect(response).to redirect_to(categories_path)
+      category.reload
+      expect(category.name).to eq "Bar"
+    end
+  end
+end
