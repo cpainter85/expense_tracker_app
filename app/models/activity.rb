9516b1992fbaec8c8e59fa1952988df787d779a1
Charles Painter <cpainter85@gmail.com>
2018-07-29 15:22:46 -0400

    Adds after_commits to activity for elasticsearch indexing

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,10 @@
 class Activity < ApplicationRecord
   include ElasticsearchActivity
 
+  after_commit -> { __elasticsearch__.index_document }, on: :create
+  after_commit -> { __elasticsearch__.update_document }, on: :update
+  after_commit -> { __elasticsearch__.delete_document }, on: :destroy
+
   belongs_to :account
   belongs_to :category
 end
