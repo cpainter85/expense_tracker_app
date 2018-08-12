2e78c77c20bdce8aac73ca793fe88e0553bae348
Charles Painter <cpainter85@gmail.com>
2018-08-12 15:31:51 -0400

    Adds #update_activity_documents

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ApplicationRecord
   has_many :activities
+
+  after_commit :update_activity_documents, on: :update
+
+  def update_activity_documents
+    Activity.bulk_update_activity_documents(
+      activity_ids: activities.pluck(:id),
+      data: { category: CategorySerializer.new(self).as_json }
+    )
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -4,4 +4,16 @@ describe Account do
   describe "relationships" do
     it { should have_many(:activities) }
   end
+
+  describe '#update_activity_documents' do
+    let(:account) { create(:account) }
+    let!(:activities) { create_list(:activity, 3, account: account) }
+
+    it "calls Activity.bulk_update_activity_documents with activities belonging to the account and the serialized account" do
+      expect(Activity).to receive(:bulk_update_activity_documents)
+        .with(activity_ids: activities.pluck(:id), data: { account: AccountSerializer.new(account).as_json })
+
+      account.update_activity_documents
+    end
+  end
 end
