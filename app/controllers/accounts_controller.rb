9001184aa363eee32b21343d1e1d45feae9d7854
Charles Painter <cpainter85@gmail.com>
2018-10-14 15:22:20 -0400

    Flash error and re-render form when account is invalid

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -10,16 +10,19 @@ class AccountsController < ApplicationController
     @activities = @response.results.map(&:_source)
     @total_balance = @response.aggregations.total_balance.value
   end
-  
+
   def new
     @account = Account.new
   end
-  
+
   def create
     @account = Account.new(account_params)
-    
+
     if @account.save
       redirect_to accounts_path, notice: "#{@account.name} was successfully added to your accounts"
+    else
+      flash.now[:error] = "Something went wrong"
+      render :new
     end
   end
 
@@ -31,11 +34,14 @@ class AccountsController < ApplicationController
     @account = Account.find(params[:id])
     if @account.update(account_params)
       redirect_to accounts_path, notice: "#{@account.name} was successfully updated"
+    else
+      flash.now[:error] = "Something went wrong"
+      render :edit
     end
   end
-  
+
   private
-  
+
   def account_params
     params.require(:account).permit(:name, :account_type, :enabled, :credit_limit)
   end
