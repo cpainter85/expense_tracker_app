6e4e643fe532110c0c23bb593b6e3839a622f97c
Charles Painter <cpainter85@gmail.com>
2018-10-20 09:43:47 -0400

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery-ui/widgets/autocomplete
+//= require autocomplete-rails
 //= require_tree .
